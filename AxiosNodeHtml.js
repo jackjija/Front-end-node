d146989efe6a7e50aa9221cf967931e0a7017cb2
jackjija <jacker1502@hotmail.com>
2024-02-14 10:11:22 +0700

    aa

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require("path");
 var bodyParser = require('body-parser');
 
 // Base URL for the API
@@ -11,6 +12,7 @@ var bodyParser = require('body-parser');
 const base_url = 'http://node58408-env-2915352.proen.app.ruk-com.cloud';
 
 // Set the template engine
+app.set("views", path.join(__dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
